74783570c5cfff63dab683f9fb97ebf401aa470d
Haydar ŞAHİN <haydarsahin34@gmail.com>
2023-06-04 22:08:16 +0300

    [fix] Remove namespace

--- src/database/migrations/2022_08_15_103302_create_cache_migrations_table.php
+++ src/database/migrations/2022_08_15_103302_create_cache_migrations_table.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace HaydarSahin\CacheMigration\Commands;
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
