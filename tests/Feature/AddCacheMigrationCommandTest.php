fdc42e6c4466e5c2df78801684cf145e853c3ad2
Haydar ŞAHİN <haydarsahin34@gmail.com>
2023-07-01 01:46:25 +0300

    [test] Include Laravel 9 and 10 to matrix tests

--- tests/Feature/AddCacheMigrationCommandTest.php
+++ tests/Feature/AddCacheMigrationCommandTest.php
@@ -15,11 +15,17 @@ class AddCacheMigrationCommandTest extends CacheMigrationBase
     public function test_file_stored_successfully()
     {
         $migrationPath = database_path('cache-migrations/2023_06_01_090000_sample_migration.php');
+        $laravelVersion = explode('.', $this->app->version())[0];
 
-        $this->artisan('make:cache-migration sampleMigration')
-            ->expectsOutput('Cache migration created successfully.')
-            ->assertExitCode(0);
-
+        if ($laravelVersion < 9) {
+            $this->artisan('make:cache-migration sampleMigration')
+                ->expectsOutput("Cache migration created successfully.")
+                ->assertExitCode(0);
+        } else {
+            $this->artisan('make:cache-migration sampleMigration')
+                ->expectsOutputToContain("created successfully.")
+                ->assertExitCode(0);
+        }
         $this->assertFileExists($migrationPath);
     }
 }
